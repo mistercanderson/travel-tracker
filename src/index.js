c2dfdba340d9592270a41e58473f617aa9f7208c
Chris Anderson <mistercanderson@gmail.com>
2021-04-21 10:54:25 -0600

    Set up basic trip card

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
-import './assets/icons/back-arrow.svg'
-import './assets/icons/drop-down-arrow.svg'
+import './images/share.svg'
+import './images/user.svg'
+import './images/menu-bars.svg'
+import './images/drop-down-arrow.svg'
+import './images/back-arrow.svg'
 
 console.log('This is the JavaScript entry file - your code begins here.');
