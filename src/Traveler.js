e12373bd4f69f07cd766e8682f7d750e13a4b384
Chris Anderson <mistercanderson@gmail.com>
2021-04-22 10:57:34 -0600

    write Traveler test/class

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,6 +1,20 @@
 class Traveler {
-  constructor() {
+  constructor({id, name, travelerType}, trips) {
+    this.id = id;
+    this.name = name;
+    this.travelerType = travelerType;
+    this.trips = trips;
+    this.getTrips();
+  }
 
+  getTrips() {
+    const myTrips = this.trips.filter(trip => trip.userID === this.id);
+    if (myTrips.length) {
+      this.trips = myTrips
+      return myTrips
+    } else {
+      return 'You don\'t have any trips'
+    }
   }
 }
 
--- test/traveler-test.js
+++ test/traveler-test.js
@@ -2,16 +2,40 @@ import chai from 'chai';
 const expect = chai.expect;
 import Traveler from '../src/Traveler';
 import travelers from './test-data/sample-traveler-data';
+import trips from './test-data/sample-trip-data'
 
 describe('Traveler', () => {
 
   let traveler;
 
   beforeEach(() => {
-    traveler = new Traveler()
+    traveler = new Traveler(travelers[0], trips)
   })
 
   it('should be an instance of Traveler', () => {
     expect(traveler).to.be.an.instanceof(Traveler)
   });
+
+  it('should have an id', () => {
+    expect(traveler.id).to.equal(1)
+  });
+
+  it('should have a name', () => {
+    expect(traveler.name).to.equal('Ham Leadbeater')
+  });
+
+  it('should have a preferred travel activity', () => {
+    expect(traveler.travelerType).to.equal('relaxer')
+  });
+
+  it('should know what trips it has been on & trips it has planned', () => {
+    expect(traveler.getTrips()).to.deep.equal([trips[3]]);
+    expect(traveler.trips).to.deep.equal([trips[3]])
+  });
+
+  it('should tell the user if they don\'t have any trips', () => {
+    const traveler2 = new Traveler(travelers[1], trips);
+
+    expect(traveler2.getTrips()).to.equal('You don\'t have any trips');
+  })
 });
