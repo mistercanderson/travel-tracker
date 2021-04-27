9883aac37c49a6e9b634d1e9ce3ecd66572a0a89
Chris Anderson <mistercanderson@gmail.com>
2021-04-27 16:38:42 -0600

    remove unused images

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import './css/base.scss';
 
-import './images/share.svg'
-import './images/user.svg'
-import './images/menu-bars.svg'
-import './images/drop-down-arrow.svg'
-import './images/back-arrow.svg'
 import './images/island.svg'
 import './images/suitcase.svg'
 import './images/plane.svg'
